b7f8b1a9aea2111474a1ce7ac135b944fdfbea1c
Themos Papatheofanous <themos360@gmail.com>
2025-11-28 00:01:49 +0200

    Updating benchmarks

--- assets/js/part2.js
+++ assets/js/part2.js
@@ -7,6 +7,7 @@ const BENCHMARK_FILES = [
     'results_fast_arm64_2025-11-22_23-24-14.json',
     'results_fast_x86_64_2025-11-23_03-45-51.json',
     'results_faster_arm64_2025-11-27_20-53-31.json',
+    'results_faster_arm64_2025-11-27_23-53-43.json',
     'results_faster_x86_64_2025-11-27_20-19-59.json',
     'results_unchained_arm64_2025-11-27_02-20-36.json',
     'results_unchained_arm64_2025-11-27_03-09-13.json',
