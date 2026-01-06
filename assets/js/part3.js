73897cd4ed33d0c640391f9093b30ee68491cccb
themosgit <themos360@gmail.com>
2026-01-07 00:00:48 +0200

    updating benchmarks

--- assets/js/part3.js
+++ assets/js/part3.js
@@ -7,11 +7,12 @@ const BENCHMARK_FILES = [
     'results_faster_arm64_2026-01-06_22-51-20.json',
     'results_faster_arm64_2026-01-06_22-56-11.json',
     'results_parallel-build_arm64_2025-12-29_12-36-21.json',
+    'results_parallel-build_x86_64_2026-01-06_23-23-37.json',
     'results_parallel-materialization_arm64_2025-12-30_12-15-00.json',
+    'results_parallel-materialization_x86_64_2026-01-06_23-50-58.json',
     'results_parallel-probe_arm64_2026-01-02_23-55-15.json',
     'results_serial_arm64_2025-12-05_04-24-09.json',
-    'results_serial_x86_64_2025-11-30_16-27-40.json',
-    'results_parallel-build_x86_64_2026-01-06_23-23-37.json'
+    'results_serial_x86_64_2025-11-30_16-27-40.json'
 ];
 
 // Color palettes (replacing D3 color scales)
