3c7bd5d8d8f0911b8e85f9b0ba398262c32c96cb
themosgit <themos360@gmail.com>
2026-01-07 00:20:24 +0200

    updating benchmarks

--- assets/js/part3.js
+++ assets/js/part3.js
@@ -11,6 +11,7 @@ const BENCHMARK_FILES = [
     'results_parallel-materialization_arm64_2025-12-30_12-15-00.json',
     'results_parallel-materialization_x86_64_2026-01-06_23-50-58.json',
     'results_parallel-probe_arm64_2026-01-02_23-55-15.json',
+    'results_parallel-probe_x86_64_2026-01-07_00-07-21.json',
     'results_serial_arm64_2025-12-05_04-24-09.json',
     'results_serial_x86_64_2025-11-30_16-27-40.json'
 ];
