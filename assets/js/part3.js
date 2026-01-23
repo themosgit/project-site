f701d7637353a473ee0ef39b47e43349dccc0c64
themosgit <themos360@gmail.com>
2026-01-24 00:42:54 +0200

    Updating benchmarks

--- assets/js/part3.js
+++ assets/js/part3.js
@@ -8,6 +8,7 @@ const BENCHMARK_FILES = [
     'results_faster_arm64_2026-01-06_22-56-11.json',
     'results_faster_arm64_2026-01-20_03-38-49.json',
     'results_faster_x86_64_2026-01-07_00-24-30.json',
+    'results_faster_x86_64_2026-01-24_00-36-48.json',
     'results_parallel-build_arm64_2025-12-29_12-36-21.json',
     'results_parallel-build_x86_64_2026-01-06_23-23-37.json',
     'results_parallel-materialization_arm64_2025-12-30_12-15-00.json',
